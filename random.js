c6e5882a25e5844b1840b852e62ed14a04a65b10
lianiverson <xls199228@gmail.com>
2015-04-20 21:18:27 +0200

    random version 1

--- random.js
+++ random.js
@@ -1,8 +1,8 @@
 //math random()
 var numero = Math.random();
+var str='Mayor que 0,5';
 
 if(numer<=0.5){
-  console.log('\n'+numero+'menor que 0,5 \n');
-}else{
-console.log('\n'+numero+'mayor que 0,5 \n');
+  str='Menos que 0,5';
 }
+console.log('\n'+numero+'mayor que 0,5 \n');
